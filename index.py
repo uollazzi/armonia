07557302e058ffd86195f2ad385ab78e2a29aa65
uollazzi <dilloawallace@hotmail.com>
2018-05-17 12:55:30 +0200

    .

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
-NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-
-MAJOR = [2, 4, 5, 7, 9, 11]
+import modes
+import note_names
 
 START_NOTE = 2
+NOTES = note_names.NOTES_FLAT
 
+# PRINT THE SCALE
 print(NOTES[START_NOTE])
-for i in MAJOR:
+for i in modes.DORIAN:
     next_note = START_NOTE + i
-    if next_note >= len(NOTES):
+    if next_note >= 12:
         next_note = next_note - 12
     print(NOTES[next_note])
---
+++ note_names.py
@@ -0,0 +1,2 @@
+NOTES_SHARP = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+NOTES_FLAT = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
